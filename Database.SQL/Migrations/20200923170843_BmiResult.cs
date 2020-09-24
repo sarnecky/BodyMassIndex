5e0410b96c85b3ee9f5dce796cebe8ecffd152bb
Sebastian Sarnecki-EXT <sebastian.sarnecki@jeppesen.com>
2020-09-24 10:08:39 +0200

    changes

---
+++ Database.SQL/Migrations/20200923170843_BmiResult.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.SQL.Migrations
+{
+    public partial class BmiResult : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BmiResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(maxLength: 100, nullable: false),
+                    Height = table.Column<int>(nullable: false),
+                    Weight = table.Column<int>(nullable: false),
+                    Bmi = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BmiResults", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BmiResults_FirstName_LastName",
+                table: "BmiResults",
+                columns: new[] { "FirstName", "LastName" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BmiResults");
+        }
+    }
+}
